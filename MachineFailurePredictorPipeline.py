4ce5ff0a35564a89702ed8604dbb62bd5d8ee539
Eric Zhou <zdog74721@gmail.com>
2023-04-06 10:27:15 -0400

    updated train_test_split to use 678 datapoints instead

--- MachineFailurePredictorPipeline.py
+++ MachineFailurePredictorPipeline.py
@@ -20,18 +20,18 @@ def main():
 
     # Use RandomUnderSampler to get 339 datapoints with label 0 and 1
     sampler = RandomUnderSampler(sampling_strategy={0: 339, 1: 339})
-    X_resampled, y_resampled = sampler.fit_resample(X, Y)
+    X_resampled, Y_resampled = sampler.fit_resample(X, Y)
 
-    print(Counter(y_resampled))  # After the filtering - TEMPORARY PRINT
+    print(Counter(Y_resampled))  # After the filtering - TEMPORARY PRINT
 
-    resampled_rows = [header] + [list(X_resampled[i]) + [y_resampled[i]] for i in range(len(X_resampled))]
+    resampled_rows = [header] + [list(X_resampled[i]) + [Y_resampled[i]] for i in range(len(X_resampled))]
 
     # Printing the resampled data - TEMPORARY PRINT
     for row in resampled_rows:
         print(row)
 
     # Split the data into 70% training and 30% testing sets
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+    X_train, X_test, Y_train, Y_test = train_test_split(X_resampled, Y_resampled, test_size=0.3, random_state=42)
 
     # Printing split results - TEMPORARY PRINTS
     print("X_train:", X_train)
